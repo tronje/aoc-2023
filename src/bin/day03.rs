5686e9c05ace5c79240628f2faf6a2ac55782013
Tronje Krabbe <hi@tron.je>
2023-12-04 17:06:05 +0100

    Day 03: Part one solution

---
+++ src/bin/day03.rs
@@ -0,0 +1,22 @@
+use anyhow::Result;
+use aoc_2023::day03::EngineSchematic;
+use std::fs::File;
+use std::io::BufReader;
+
+const PATH: &str = "inputs/day03/input";
+
+fn part_one() -> Result<u32> {
+    let f = File::open(PATH)?;
+    let reader = BufReader::new(f);
+
+    let schematic = EngineSchematic::load(reader)?;
+    let part_nos = schematic.part_numbers();
+
+    let result = part_nos.iter().map(|part_no| part_no.num).sum();
+    Ok(result)
+}
+
+fn main() {
+    let part_no_sum = part_one().unwrap();
+    println!("Part one: {part_no_sum}");
+}
---
+++ src/day03.rs
@@ -0,0 +1,148 @@
+use anyhow::Result;
+use std::collections::HashSet;
+use std::hash::Hash;
+use std::io::prelude::*;
+use std::ops::Range;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub struct FixedAddress {
+    x: u32,
+    y: u32,
+}
+
+impl FixedAddress {
+    pub fn new(x: u32, y: u32) -> Self {
+        Self { x, y }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Address {
+    x: Range<u32>,
+    y: u32,
+}
+
+impl Address {
+    fn new(x_start: u32, x_end: u32, y: u32) -> Self {
+        Self {
+            x: Range {
+                start: x_start,
+                end: x_end,
+            },
+            y,
+        }
+    }
+
+    fn is_adjacent_to(&self, other: &FixedAddress) -> bool {
+        if self.y.abs_diff(other.y) > 1 {
+            return false;
+        }
+
+        if self.x.contains(&other.x) {
+            return true;
+        }
+
+        if self.x.start > other.x && (self.x.start - other.x) <= 1 {
+            return true;
+        }
+
+        if self.x.end <= other.x && (other.x - self.x.end) == 0 {
+            return true;
+        }
+
+        false
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Number {
+    pub num: u32,
+    addr: Address,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub struct Symbol {
+    sym: char,
+    addr: FixedAddress,
+}
+
+#[derive(Debug)]
+pub struct EngineSchematic {
+    numbers: Vec<Number>,
+    symbols: Vec<Symbol>,
+}
+
+impl EngineSchematic {
+    pub fn load<R>(reader: R) -> Result<Self>
+    where
+        R: BufRead,
+    {
+        let mut numbers = Vec::new();
+        let mut symbols = Vec::new();
+
+        let mut in_number = false;
+        let mut num_x_start = 0;
+
+        let mut y = 0;
+
+        let mut buf = String::new();
+
+        for line in reader.lines() {
+            let line = line?;
+
+            let mut x = 0;
+
+            for c in line.chars() {
+                if c.is_ascii_digit() {
+                    buf.push(c);
+
+                    if !in_number {
+                        in_number = true;
+                        num_x_start = x;
+                    }
+
+                    x += 1;
+                    continue;
+                }
+
+                if in_number {
+                    let num = buf.parse::<u32>()?;
+                    let addr = Address::new(num_x_start, x, y);
+                    numbers.push(Number { num, addr });
+
+                    in_number = false;
+                    buf.clear();
+                }
+
+                if c == '.' {
+                    x += 1;
+                    continue;
+                }
+
+                // found a symbol
+                let addr = FixedAddress::new(x, y);
+                symbols.push(Symbol { sym: c, addr });
+
+                x += 1;
+            }
+
+            y += 1;
+        }
+
+        Ok(Self { numbers, symbols })
+    }
+
+    pub fn part_numbers(&self) -> HashSet<Number> {
+        let mut part_nos = HashSet::new();
+
+        for symbol in self.symbols.iter() {
+            for number in self.numbers.iter() {
+                if number.addr.is_adjacent_to(&symbol.addr) {
+                    part_nos.insert(number.clone());
+                }
+            }
+        }
+
+        part_nos
+    }
+}
