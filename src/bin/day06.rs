ef0e7caa5dec5b16e949f7ada63180c22fa8c8a0
Tronje Krabbe <hi@tron.je>
2023-12-06 09:14:52 +0100

    Day 06: Part two solution

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -9,12 +9,23 @@ fn part_one() -> Result<u64> {
     let f = File::open(PATH)?;
     let reader = BufReader::new(f);
 
-    let races = Race::parse(reader)?;
+    let races = Race::parse_p1(reader)?;
     let result = races.iter().map(Race::n_winning_holds).product();
     Ok(result)
+}
+
+fn part_two() -> Result<u64> {
+    let f = File::open(PATH)?;
+    let reader = BufReader::new(f);
+
+    let race = Race::parse_p2(reader)?;
+    Ok(race.solve())
 }
 
 fn main() {
     let result = part_one().unwrap();
     println!("Part one: {result}");
+
+    let result = part_two().unwrap();
+    println!("Part two: {result}");
 }
--- src/day06.rs
+++ src/day06.rs
@@ -21,6 +21,18 @@ impl Race {
         n
     }
 
+    pub fn solve(&self) -> u64 {
+        let time = self.time as f64;
+        let distance = self.distance as f64;
+
+        let sqrt = (time.powi(2) - 4.0 * distance).sqrt();
+
+        let lower = (-time - sqrt) / 2.0;
+        let upper = (-time + sqrt) / 2.0;
+
+        (upper.ceil() - lower.floor()) as u64 - 1
+    }
+
     fn parse_numbers(s: &str) -> Vec<u64> {
         let mut n = 0;
         let mut factor = 1;
@@ -40,7 +52,21 @@ impl Race {
         ns
     }
 
-    pub fn parse<R>(reader: R) -> Result<Vec<Self>>
+    fn parse_number(s: &str) -> u64 {
+        let mut n = 0;
+        let mut factor = 1;
+
+        for chr in s.chars().rev() {
+            if let Some(digit) = chr.to_digit(10) {
+                n += digit as u64 * factor;
+                factor *= 10;
+            }
+        }
+
+        n
+    }
+
+    pub fn parse_p1<R>(reader: R) -> Result<Vec<Self>>
     where
         R: BufRead,
     {
@@ -55,5 +81,43 @@ impl Race {
             .collect();
 
         Ok(races)
+    }
+
+    pub fn parse_p2<R>(reader: R) -> Result<Self>
+    where
+        R: BufRead,
+    {
+        let mut lines = reader.lines();
+        let time = Self::parse_number(&lines.next().context("invalid input")??);
+        let distance = Self::parse_number(&lines.next().context("invalid input")??);
+
+        Ok(Self { time, distance })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Race;
+    use std::io::BufReader;
+
+    const EXAMPLE: &str = "Time:      7  15   30\nDistance:  9  40  200";
+
+    #[test]
+    fn test_examples() {
+        // part one
+        let races = Race::parse_p1(BufReader::new(EXAMPLE.as_bytes())).unwrap();
+        let naive_result = races.iter().map(Race::n_winning_holds).product::<u64>();
+        let smart_result = races.iter().map(Race::solve).product();
+
+        assert_eq!(naive_result, 288);
+        assert_eq!(naive_result, smart_result);
+
+        // part two
+        let race = Race::parse_p2(BufReader::new(EXAMPLE.as_bytes())).unwrap();
+        let naive_result = race.n_winning_holds();
+        let smart_result = race.solve();
+
+        assert_eq!(naive_result, 71503);
+        assert_eq!(naive_result, smart_result);
     }
 }
